8c41ba4f5d0c0e2102ae9bd1f4cfe5dc921bfaf4
violet53712 <cli893@gatech.edu>
2026-01-25 15:44:24 -0500

    fucntions

--- pyaedt/generatelens.py
+++ pyaedt/generatelens.py
@@ -15,10 +15,26 @@ k = math.sqrt(3.1496)
 tf = w/(4*(k-1))
 for i in range(0, 4):
     hfss["h" + str(i)] = i*tf + 0.001
-rings.append(hfss.modeler.create_cylinder(orientation = "XY", origin = (0, 0, 0), radius = "r"+str(1), height = ("h" + str(3)), name = "h" + str(1), material="Plastic, PLA"))
+rings.append(hfss.modeler.create_cylinder(orientation = "XY", 
+                                          origin = (0, 0, 0), 
+                                          radius = "r"+str(1), 
+                                          height = ("h" + str(3)), 
+                                          name = "h" + str(1), 
+                                          material="Plastic, PLA"))
 for n in range(2, 17):
     i = (16-n)%4
-    rings.append(hfss.modeler.create_cylinder(orientation = "XY", origin = (0, 0, 0), radius = "r"+str(n), height = ("h" + str(i)), name = "h" + str(n), material="Plastic, PLA"))
-    delete.append(hfss.modeler.create_cylinder(orientation = "XY", origin = (0, 0, 0), radius = "r"+str(n-1), height = ("h" + str(i)), name = "delete" + str(n)))
+    rings.append(hfss.modeler.create_cylinder(orientation = "XY", 
+                                              origin = (0, 0, 0), 
+                                              radius = "r"+str(n), 
+                                              height = ("h" + str(i)), 
+                                              name = "h" + str(n), 
+                                              material="Plastic, PLA",
+                                              num_sides= 20))
+    delete.append(hfss.modeler.create_cylinder(orientation = "XY", 
+                                               origin = (0, 0, 0), 
+                                               radius = "r"+str(n-1), 
+                                               height = ("h" + str(i)), 
+                                               name = "delete" + str(n), 
+                                               num_sides=20))
     rings[n-1].subtract(delete[n-2])
     hfss.modeler["delete"+str(n)].delete()
